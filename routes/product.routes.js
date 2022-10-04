976152277a2acefd7f0f7bb1605eb37da99bca89
Eurico Moutinho <yungsniper@hotmail>
2022-10-04 15:02:19 +0100

    e-commerce

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -11,5 +11,11 @@ router.get("/product/slug/:slug", (req, res, next) => {
   res.send(product);}else{res.status(404).send({message: 'Product Not Found'})}
 });
 
+router.get("/product/:slug", (req, res, next) => {
+  const product = data.find(e => {return (e.slug == req.params.slug)})
+  if(product){
+  res.send(product);}else{res.status(404).send({message: 'Product Not Found'})}
+});
+
 
 module.exports = router;
